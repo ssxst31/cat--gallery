9c6b2abac6a91f2dcda3c9b7ee6f059ad939d6e0
star <chlvkfksquf@gmail.com>
2022-04-07 03:02:59 +0900

    Add development environment

---
+++ src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Main() {
+	return <Wrapper>Main</Wrapper>;
+}
+
+const Wrapper = styled.div``;
+
+export default Main;
