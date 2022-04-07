a39120407dff0c2a8e2de1a5c2652a6f94f5dca2
star <chlvkfksquf@gmail.com>
2022-04-08 01:48:09 +0900

    Add mock data name

--- src/api/catPhoto/data.tsx
+++ src/api/catPhoto/data.tsx
@@ -3,150 +3,187 @@ import { CatPhoto } from '../../type';
 const catPhotoList: CatPhoto[] = [
   {
     id: 1,
+    name: 'Abyssinian',
     image: 'https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg',
   },
   {
     id: 2,
+    name: 'Aegean',
     image: 'https://cdn2.thecatapi.com/images/ozEvzdVM-.jpg',
   },
   {
     id: 3,
+    name: 'American Bobtail',
     image: 'https://cdn2.thecatapi.com/images/hBXicehMA.jpg',
   },
   {
     id: 4,
+    name: 'American Curl',
     image: 'https://cdn2.thecatapi.com/images/xnsqonbjW.jpg',
   },
   {
     id: 5,
+    name: 'American Shorthair',
     image: 'https://cdn2.thecatapi.com/images/JFPROfGtQ.jpg',
   },
   {
     id: 6,
+    name: 'American Wirehair',
     image: 'https://cdn2.thecatapi.com/images/8D--jCd21.jpg',
   },
   {
     id: 7,
+    name: 'Arabian Mau',
     image: 'https://cdn2.thecatapi.com/images/k71ULYfRr.jpg',
   },
   {
     id: 8,
+    name: 'Australian Mist',
     image: 'https://cdn2.thecatapi.com/images/_6x-3TiCA.jpg',
   },
   {
     id: 9,
+    name: 'Balinese',
     image: 'https://cdn2.thecatapi.com/images/13MkvUreZ.jpg',
   },
   {
     id: 10,
+    name: 'Bambino',
     image: 'https://cdn2.thecatapi.com/images/5AdhMjeEu.jpg',
   },
   {
     id: 11,
+    name: 'Bengal',
     image: 'https://cdn2.thecatapi.com/images/O3btzLlsO.png',
   },
   {
     id: 12,
+    name: 'Birman',
     image: 'https://cdn2.thecatapi.com/images/HOrX5gwLS.jpg',
   },
   {
     id: 13,
+    name: 'Bombay',
     image: 'https://cdn2.thecatapi.com/images/5iYq9NmT1.jpg',
   },
   {
     id: 14,
+    name: 'British Longhair',
     image: 'https://cdn2.thecatapi.com/images/7isAO4Cav.jpg',
   },
   {
     id: 15,
+    name: 'British Shorthair',
     image: 'https://cdn2.thecatapi.com/images/s4wQfYoEk.jpg',
   },
   {
     id: 16,
+    name: 'Burmese',
     image: 'https://cdn2.thecatapi.com/images/4lXnnfxac.jpg',
   },
   {
     id: 17,
+    name: 'Burmilla',
     image: 'https://cdn2.thecatapi.com/images/jvg3XfEdC.jpg',
   },
   {
     id: 18,
+    name: 'California Spangled',
     image: 'https://cdn2.thecatapi.com/images/B1ERTmgph.jpg',
   },
   {
     id: 19,
+    name: 'Chantilly-Tiffany',
     image: 'https://cdn2.thecatapi.com/images/s4wQfYoEk.jpg',
   },
   {
     id: 20,
+    name: 'Chartreux',
     image: 'https://cdn2.thecatapi.com/images/TR-5nAd_S.jpg',
   },
   {
     id: 21,
+    name: 'Chartreux',
     image: 'https://cdn2.thecatapi.com/images/j6oFGLpRG.jpg',
   },
   {
     id: 22,
+    name: 'Chausie',
     image: 'https://cdn2.thecatapi.com/images/vJ3lEYgXr.jpg',
   },
   {
     id: 23,
+    name: 'Cheetoh',
     image: 'https://cdn2.thecatapi.com/images/IFXsxmXLm.jpg',
   },
   {
     id: 24,
+    name: 'Colorpoint Shorthair',
     image: 'https://cdn2.thecatapi.com/images/oSpqGyUDS.jpg',
   },
   {
     id: 25,
+    name: 'Cornish Rex',
     image: 'https://cdn2.thecatapi.com/images/unX21IBVB.jpg',
   },
   {
     id: 26,
+    name: 'Cymric',
     image: 'https://cdn2.thecatapi.com/images/3dbtapCWM.jpg',
   },
   {
     id: 27,
+    name: 'Cyprus',
     image: 'https://cdn2.thecatapi.com/images/tJbzb7FKo.jpg',
   },
   {
     id: 28,
+    name: 'Devon Rex',
     image: 'https://cdn2.thecatapi.com/images/4RzEwvyzz.png',
   },
   {
     id: 29,
+    name: 'Donskoy',
     image: 'https://cdn2.thecatapi.com/images/3KG57GfMW.jpg',
   },
   {
     id: 30,
+    name: 'Dragon Li',
     image: 'https://cdn2.thecatapi.com/images/BQMSld0A0.jpg',
   },
   {
     id: 31,
+    name: 'Egyptian Mau',
     image: 'https://cdn2.thecatapi.com/images/TuSyTkt2n.jpg',
   },
   {
     id: 32,
+    name: 'European Burmese',
     image: 'https://cdn2.thecatapi.com/images/njK25knLH.jpg',
   },
   {
     id: 33,
+    name: 'Exotic Shorthair',
     image: 'https://cdn2.thecatapi.com/images/CDhOtM-Ig.jpg',
   },
   {
     id: 34,
+    name: 'Havana Brown',
     image: 'https://cdn2.thecatapi.com/images/-tm9-znzl.jpg',
   },
   {
     id: 35,
+    name: 'Himalayan',
     image: 'https://cdn2.thecatapi.com/images/xoI_EpOKe.jpg',
   },
   {
     id: 36,
+    name: 'Japanese Bobtail',
     image: 'https://cdn2.thecatapi.com/images/165ok6ESN.jpg',
   },
   {
     id: 37,
+    name: 'Javanese',
     image: 'https://cdn2.thecatapi.com/images/DbwiefiaY.png',
   },
 ];
