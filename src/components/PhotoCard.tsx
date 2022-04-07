1594b08d134d29c38e028277207a6efcd55d663f
star <chlvkfksquf@gmail.com>
2022-04-08 01:50:25 +0900

    Modify PhotoCard css

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function PhotoCard() {
+interface PhotoCardProps {
+  image: string;
+  name: string;
+}
+
+function PhotoCard({ image, name }: PhotoCardProps) {
   return (
     <PhotoCardWrapper>
-      <PhotoTitle />
-      <PhotoImage />
+      <PhotoTitle>{name}</PhotoTitle>
+      <PhotoImage image={image} />
     </PhotoCardWrapper>
   );
 }
-
-const PhotoCardWrapper = styled.div`
-  position: relative;
-  line-height: 0;
-  overflow: hidden;
-`;
 
 const PhotoTitle = styled.span`
   z-index: 1;
@@ -24,20 +23,17 @@ const PhotoTitle = styled.span`
   width: 100%;
   height: 100%;
   background: rgba(90, 0, 10, 0.4);
-
   color: #fff;
-  font-size: 1.5em;
+  font-size: 24px;
   font-weight: bold;
-
   display: flex;
   align-items: center;
   justify-content: center;
-
   opacity: 0;
   transition: opacity 0.3s;
 `;
 
-const PhotoImage = styled.img`
+const PhotoImage = styled.img.attrs<{ image: string }>((props) => ({ src: props.image }))<{ image: string }>`
   width: 100%;
   height: auto;
   object-fit: cover;
@@ -46,8 +42,18 @@ const PhotoImage = styled.img`
   transform: scale(1.1);
 `;
 
-PhotoImage.defaultProps = {
-  src: 'https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg',
-};
+const PhotoCardWrapper = styled.div`
+  position: relative;
+  line-height: 0;
+  overflow: hidden;
+
+  &:hover ${PhotoTitle} {
+    opacity: 1;
+  }
+
+  &:hover ${PhotoImage} {
+    filter: blur(2px);
+  }
+`;
 
 export default PhotoCard;
